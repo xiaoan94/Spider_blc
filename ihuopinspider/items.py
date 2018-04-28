02d0212c9723832fd2f1dbf02a993a718dc03350
liushouhua <liushouhua@didachuxing.com>
2018-04-28 22:13:13 +0800

    item

--- ihuopinspider/items.py
+++ ihuopinspider/items.py
@@ -5,10 +5,17 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item,Field
 
 
-class IhuopinspiderItem(scrapy.Item):
+class IhuopinspiderItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    #新闻标题
+    title = Field()
+    #作者
+    author=Field()
+    #时间
+    time = Field()
+    #内容
+    
